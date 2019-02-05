a926b5e828f34f8a89087180e8c4ad9562356e1d
Richard Afro <richyafro@hotmail.com>
2019-02-05 12:49:37 +0100

    Change location of import script

--- export.sh
+++ export.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+ENV_FILE=$1
+
 while read line; do
 echo export ${line} >> $BASH_ENV;
-done < ./trial/.env;
+done < ${ENV_FILE};
 echo "EXPORTED VARIABLES INTO BASH ENVIRONMENT";
